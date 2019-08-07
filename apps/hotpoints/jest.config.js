cc739ee7d3aa48c55cde2512ac907d2d39d0f1d9
Nicolai Ghersun <ghershun@gmail.com>
2019-08-07 18:38:31 +0300

    Added skeleton for react/nest hotpoints application.

---
+++ apps/hotpoints/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'hotpoints',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/hotpoints'
+};
